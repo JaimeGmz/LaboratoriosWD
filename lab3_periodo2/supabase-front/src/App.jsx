e22444a97e89894201e3baa66b0e813fb5f548b0
JaimeGmz <149437037+JaimeGmz@users.noreply.github.com>
2026-04-09 19:28:27 -0600

    supabase tabla propia

--- lab3_periodo2/supabase-front/src/App.jsx
+++ lab3_periodo2/supabase-front/src/App.jsx
@@ -1,121 +1,101 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useEffect, useState } from "react";
+import "./App.css";
+import supabase from '../supabase.config.js';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todoList, setTodoList] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    consulta();
+  }, []);
+
+  const consulta = async () => {
+    const { data, error } = await supabase.from("ejercicios").select("*");
+    console.log(data);
+    if (error) {
+      console.log("Error de conexion en consulta: ", error);
+    } else {
+      setTodoList(data);
+    }
+  };
+
+  const fetchTodos = async () => {
+    const { data, error } = await supabase.from("ejercicios").select("*");
+    if (error) {
+      console.log("Error fetching: ", error);
+    } else {
+      setTodoList(data);
+    }
+  };
+  const addTodo = async () => {
+    const newTodoData = {
+      tipo_ejercicio: newTodo,
+      terminado: false,
+    };
+    const { data, error } = await supabase
+      .from("ejercicios")
+      .insert([newTodoData])
+      .single();
+    if (error) {
+      console.log("Error adding todo: ", error);
+    } else {
+      setTodoList((prev) => [...prev, data]);
+      setNewTodo("");
+    }
+  };
+  const completeTask = async (id, terminado) => {
+    const { data, error } = await supabase
+      .from("ejercicios")
+      .update({ terminado: !terminado })
+      .eq("id", id);
+    if (error) {
+      console.log("error toggling task: ", error);
+    } else {
+      const updatedTodoList = todoList.map((todo) =>
+        todo.id === id ? { ...todo, terminado: !terminado } : todo
+      );
+      setTodoList(updatedTodoList);
+    }
+  };
+  const deleteTask = async (id) => {
+    const { data, error } = await supabase
+      .from("ejercicios")
+      .delete()
+      .eq("id", id);
+    if (error) {
+      console.log("error deleting task: ", error);
+    } else {
+      setTodoList((prev) => prev.filter((todo) => todo.id !== id));
+    }
+  };
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
-  )
+    <div>
+      {" "}
+      <h1>Todo List</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="New Todo..."
+          value={newTodo}
+          onChange={(e) => setNewTodo(e.target.value)}
+        />
+        <button onClick={addTodo}> Add Todo Item</button>
+      </div>
+      <ul>
+        {todoList.map((todo) => (
+          <li>
+            <p> {todo.tipo_ejercicio}</p>
+            <button onClick={() => completeTask(todo.id, todo.terminado)}>
+              {" "}
+              {todo.terminado ? "Undo" : "Complete Task"}
+            </button>
+            <button onClick={() => deleteTask(todo.id)}> Delete Task</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
-
-export default App
+export default App;
